7e1b86f9da63ebb16ea2a79994fb5aa3e4971299
ktg19 <gkato@granvalley.co.jp>
2020-06-14 20:07:05 +0900

    comment

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+  <v-card-text>
+      <v-row align="center" v-touch:swipe.left="() => alert(11)"> 
+        <!-- v-touch:swipe.left="() => alert(11)" -->
+        <v-checkbox
+          v-model="includeFiles"
+          hide-details
+          class="shrink mr-2 mt-0"
+        ></v-checkbox>
+        aaaaaa
+        <v-text-field label="New Task"></v-text-field>
+      </v-row>
+    </v-card-text>
+  </v-layout>
+</template>
+
+<script>
+import Vue from 'vue'
+import Vue2TouchEvents from 'vue2-touch-events'
+ 
+Vue.use(Vue2TouchEvents)
+
+export default {
+  components: {}
+}
+</script>
